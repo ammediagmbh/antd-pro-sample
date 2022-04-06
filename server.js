740e3a18afcbb45ed33d457542cfbb3c48fb6b18
ammediagmbh <react-app>
2022-04-06 16:27:24 +0200

    Updated server.js.

--- server.js
+++ server.js
@@ -7,8 +7,12 @@ const PORT = process.env.PORT || 3000;
 
 // App
 const app = express();
-app.get('/', function (req, res) {
-  res.send('Hello world\n');
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(PORT);

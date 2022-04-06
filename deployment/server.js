941dc0564af2ac71fb8c3312868e84e8065ea499
ammediagmbh <react-app>
2022-04-06 16:30:11 +0200

    Updated server.js.

--- deployment/server.js
+++ deployment/server.js
@@ -12,7 +12,7 @@ app.use(express.static(path.join(__dirname, 'build')));
 
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'dist/index.html'));
 });
 
 app.listen(PORT);
